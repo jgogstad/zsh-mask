2139dd504fcc6a5626ea8434991c274621bbfb69
Jostein Gogstad <jostein.gogstad@gmail.com>
2020-05-05 09:25:12 +0200

    Don't switch history context

--- passwordless-history.plugin.zsh
+++ passwordless-history.plugin.zsh
@@ -1,13 +1,12 @@
 # ZSH
 
-HISTORY_EXCLUDE_PATTERN="^ |password|//[^/]+:[^/]+@|(KEY|TOKEN|SECRET)=|Authorization:"
+HISTORY_EXCLUDE_PATTERN="^ |password|//[^/]+:[^/]+@|(KEY|TOKEN|SECRET)=|Authorization: ?[^\$]"
 
 function zshaddhistory() {
   emulate -L zsh
   unsetopt case_match
   if ! [[ "$1" =~ $HISTORY_EXCLUDE_PATTERN ]] ; then
       print -Sr -- "${1%%$'\n'}"
-      fc -p
   else
       return 1
   fi
